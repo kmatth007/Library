b89db3ed35398300e8c82f8f73e5ba275e683f38
kmatth007 <kmatth007@yahoo.com>
2014-11-19 12:03:12 -0500

    new updates

--- Library.Model/Book.cs
+++ Library.Model/Book.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Library.Infrastructure;
-
 namespace Library.Model
 {
 	public class Book : IAggregrateRoot
 	{
-		public Guid Id { get; set; }
+		
+      
+        public Guid Id { get; set; }
 		public virtual BookTitle Title { get; set; }
 		public virtual Member OnLoanTo { get; set; }
 
--- Library.Model/BookTitle.cs
+++ Library.Model/BookTitle.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Library.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Model
 {
 	public class BookTitle : IAggregrateRoot
 	{
+        [Key]
 		public string ISBN { get; set; }
 		public string Title { get; set; }
 
